815017e0eed2c888a6220a6b11bff8cea02a241e
Gervasio Marchand <github@gervas.io>
2020-07-22 10:28:26 -0300

    It _just_ needs making the existing regex case insensitive :)

--- injected_script.js
+++ injected_script.js
@@ -13,10 +13,7 @@ let defaultFaviconHref = favicon.href;
 defaultFaviconHref = defaultFaviconHref.replace(/^https?:/, '');
 
 // now force the cdn
-defaultFaviconHref = defaultFaviconHref.replace(/^\/\/[^\/]*\/(content\/)?/, '//cdn.sstatic.net/');
-
-// some sites have /Content in the path which does not match the cdn... remove that
-defaultFaviconHref = defaultFaviconHref.replace(/\/Content/i, '');
+defaultFaviconHref = defaultFaviconHref.replace(/^\/\/[^\/]*\/(content\/)?/i, '//cdn.sstatic.net/');
 
 // aaand use https... because we're good people
 defaultFaviconHref = 'https:' + defaultFaviconHref;
